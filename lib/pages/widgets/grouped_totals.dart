a9d228209fbbdedf941fcaee3114de4fe50002dc
Andrew Greenman <agreenman@hagerty.com>
2020-05-16 09:55:39 -0400

    Fixing layout issues with grouped_layouts.

--- lib/pages/widgets/grouped_totals.dart
+++ lib/pages/widgets/grouped_totals.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:strava_flutter/Models/activity.dart';
+import 'package:strava_stats/services/strava_service.dart';
 
 class GroupedTotals extends StatefulWidget {
   @override
@@ -6,10 +8,29 @@ class GroupedTotals extends StatefulWidget {
 }
 
 class _GroupedTotalsState extends State<GroupedTotals> {
+  bool _isLoadingActivities = true;
+  List<SummaryActivity> _activities = List<SummaryActivity>();
+  StravaService _stravaService = StravaService();
+  String _currentSort;
+  var _sorts = [
+    'Weekly',
+    'Monthly',
+    'Yearly'
+  ];
 
   @override
   void initState() {
     super.initState();
+
+    _currentSort = 'Weekly';
+
+    _stravaService.getAllActivities().then((activities) {
+      setState(() {
+        _activities = activities;
+        _isLoadingActivities = false;
+      });
+    });
+
   }
 
   @override
@@ -18,8 +39,30 @@ class _GroupedTotalsState extends State<GroupedTotals> {
       children: [
         Column(
           children: [
+            Row(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(left: 16.0, right: 8.0),
+                  child: DropdownButton<String>(
+                    value: _currentSort,
+                    icon: Icon(Icons.arrow_drop_down_circle, color: Colors.deepOrange,),
+                    items: _sorts.map((String value) {
+                      return DropdownMenuItem<String>(
+                        value: value,
+                        child: Text(value),
+                      );
+                    }).toList(),
+                    onChanged: (String selection) {
+                      setState(() {
+                        _currentSort = selection;
+                      });
+                    },
+                  ),
+                ),
+              ]
+            ),
             Expanded(
-              child: Text('Feature coming soon...'),
+              child: !_isLoadingActivities ? Text('Feature coming soon...') : Text('Loading activities...'),
             ),
           ],
         ),
